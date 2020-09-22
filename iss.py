e82d2c586d7a56b4cb45db07845515bffd6c2513
Kevin Clark <kevinwilliamclark@gmail.com>
2020-09-22 13:02:23 -0500

    starting the structure of the program

--- iss.py
+++ iss.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Kevin Clark'
+
+import requests
+
+
+def obtain_astronauts():
+    """obtains a list of astronauts, prints full names,
+    the spacecraft they are aboard, and the total number
+    of astronauts in space
+    """
+    pass
+
+
+def current_coord():
+    """obtains geographic coordinates of ISS along with
+    timestamp
+    """
+    pass
+
+
+def graphic_display():
+    """creates a graphic screen with the world map,
+    registers an icon for the ISS and moves the ISS to
+    its current lat/lon on the map"""
+    pass
 
 
 def main():
